a601ed8cbe085da50a08032d9462950595796687
Levente Hunyadi <levente.hunyadi@instructure.com>
2022-02-09 12:09:48 +0100

    Remove obsolete dependency

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setup(
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    install_requires=["json_strong_typing", "docstring_parser"],
+    install_requires=["json_strong_typing"],
 )
