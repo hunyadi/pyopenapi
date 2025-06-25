f34d7f1b0482e871cb489d949abe7abf28e4a3dd
Levente Hunyadi <hunyadi@gmail.com>
2025-06-25 16:30:22 +0200

    Publish new release to PyPI

--- pyopenapi/__init__.py
+++ pyopenapi/__init__.py
@@ -6,7 +6,7 @@ Copyright 2022-2025, Levente Hunyadi
 :see: https://github.com/hunyadi/pyopenapi
 """
 
-__version__ = "0.1.10"
+__version__ = "0.2.0"
 __author__ = "Levente Hunyadi"
 __copyright__ = "Copyright 2022-2025, Levente Hunyadi"
 __license__ = "MIT"
